45a171e6bb4f86ec788f4030715456212e9f1ed3
이경화 <leegaeng@gmail.com>
2015-04-29 16:34:43 +0900

    day1 shape Java project_ 이경화 생성

---
+++ src/Circle.java
@@ -0,0 +1,7 @@
+public class Circle extends Shape {
+
+	public void draw() {
+		System.out.println("원을 그렸습니다");
+	}
+
+}
---
+++ src/ColorPoint.java
@@ -0,0 +1,27 @@
+public class ColorPoint extends Point {
+
+	private String color;
+
+	public ColorPoint() {
+		System.out.println("칼라포인트~");
+	}
+
+	public ColorPoint(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void show() {
+		System.out.println("좌표[x=" + getX() + ", y=" + getY() + ", color="
+				+ color + "] 에 점을 그렸습니다.");
+	}
+
+}
